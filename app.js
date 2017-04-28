0bda0d79387b9b175c8688b290d049a40581a536
nathanallen <thenathanator@gmail.com>
2017-04-27 20:26:59 -0700

    draw point of motion on canvas with p5js

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ function setupCamera(){
   console.log("Inintializing Camera");
   camMotion = CamMotion.Engine();
   camMotion.on("error", function (e) {console.log("camera error", e);});
-  camMotion.on("frame", reactToMotion);
   camMotion.start();
 }
 
-function reactToMotion(){
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
   var point = camMotion.getMovementPoint(true);
   console.log("motion detected at point:", point);
+  fill(255);
+  ellipse(point.x, point.y, 20, 20);
 }
