e3fa5251f2b3e8058cc587c47b17a478bc5c1bc1
nathanallen <thenathanator@gmail.com>
2017-05-03 17:31:46 -0700

    working sine wave

--- demo/app.js
+++ demo/app.js
@@ -1,51 +1,76 @@
-var camMotion; // globals
-var song;
-var colors = [];
+/*
+ * Source: https://p5js.org/examples/math-sine-wave.html
+*/
 
-//setupCamera();
+var xspacing = 8;    // Distance between each horizontal location
+var w;                // Width of entire wave
+var theta = 0.0;      // Start angle at 0
+var amplitude = 90.0; // Height of wave
+var period = 250.0;   // How many pixels before the wave repeats
+var dx;               // Value for incrementing x
+var yvalues;  // Using an array to store height values for the wave
+var colors = []; // Array of colors
+var mySound;
 
-////
-
-function setupCamera(){
-  console.log("Inintializing Camera");
-  camMotion = CamMotion.Engine();
-  camMotion.on("error", function (e) {console.log("camera error", e);});
-  camMotion.start();
+function preload (){
+  setupSound(); // TODO: uncomment function code!
 }
 
-// function preload (){
-// 	mySound = loadSound('ASMR.mp3');
-// }
-
 function setup() {
-  createCanvas(1080, 1920);
-  // mySound.setVolume(0.1);
-  // mySound.play();
+  setupColors();
+  playSound();// TODO: uncomment function code!
+
+  createCanvas(window.outerWidth, window.outerHeight); // fills browser window
+  w = width+16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w/xspacing));
+}
+
+function draw() {
+  background(0);
+  calcWave();
+  renderWave();
+}
+
+function calcWave() {
+  // Increment theta (try different values for
+  // 'angular velocity' here)
+  theta += 0.02;
+
+  // For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x)*amplitude;
+    x+=dx;
+  }
+}
+
+function renderWave() {
+  noStroke();
+  // A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    fill(randColor());
+    ellipse(height/2+yvalues[x], x*xspacing, 8, 8);
+  }
+}
+
+function setupColors() {
   colors[0] = color(111,184,232);
   colors[1] = color(85,182,178);
   colors[2] = color(198,218,198);
   colors[3] = color(197,192,151);
   colors[4] = color(223,152,63);
-
 }
 
-function draw() {
-//  var point = camMotion.getMovementPoint(true);
-  //console.log("motion detected at point:", point);
-  strokeWeight(0);
+function randColor() {
+  return colors[int(random(0, colors.length-1))];
+}
 
-
-  //the color issue was that the random was making floats with decimals and I used round to round up to the next integer.
-  var col = round(int(random(0, 4)));
-  console.log("line of stuff ", colors[col]);
-
-  //ellipse(point.x, point.y, 4, 4);
-  //I commented out the above code and camera stuff and set the dots to appear following the mouse.
-  //since the other group is using motion capture and we only have one computer
-  //I think we should just do a visual sound piece With the grains of sand and audio
-
-  ellipse(mouseX, mouseY, 4, 4);
-
-  fill(colors[col]);
-
+// TODO: uncomment these when we're ready
+function setupSound() {
+  // mySound = loadSound('ASMR.mp3');
+}
+function playSound() {
+  // mySound.setVolume(0.1);
+  // mySound.play();
 }
