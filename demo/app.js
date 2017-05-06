cc8ea044507bb7b6206c90999c7b25944add75a9
nathanallen <thenathanator@gmail.com>
2017-05-06 14:00:46 -0700

    Revert "Background image"
    This reverts commit 1bb4d6b3f088f63207fd240adb0865f323587016.

--- demo/app.js
+++ demo/app.js
@@ -34,32 +34,6 @@ function setup() {
   playSound();// TODO: uncomment function code!
   frameRate(10);
 
-  //background image
-
-var bg;
-var y = 0;
-
-function setup() {
-  // The background image must be the same size as the parameters
-  // into the createCanvas() method. In this program, the size of
-  // the image is 720x400 pixels.
-  bg = loadImage("demo/backgroundtexture.jpg");
-  createCanvas(720, 400);
-}
-
-function draw() {
-  background(bg);
-
-  stroke(226, 204, 0);
-  line(0, y, width, y);
-
-  y++;
-  if (y > height) {
-    y = 0;
-  }
-}
-
-
 
   createCanvas(window.outerWidth, window.outerHeight); // fills browser window
   W = width+16;
